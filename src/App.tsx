079bacc6b295736fc2c95ff3816c084998111666
bl4ck-w0lf-project <hounmenouangelo@gmail.com>
2026-08-28 12:16:37 +0100

    Installation des dépendances et création de l'aborescence

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-red-700">
+        <p className="text-blue-700">Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+ 
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  prefix: "",
+  theme: {
+    
+    extend: {
+      
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+};
